2d3e39b0c01d326b830d6af800850b8c94b587bc
litao34 <litao34@58.com>
2022-03-30 18:42:13 +0800

    init

---
+++ src/DemoTest.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author litao34
+ * @ClassName DemoTest
+ * @Description TODO
+ * @CreateDate 2022/3/30-10:18 AM
+ **/
+public class DemoTest {
+    public static void main(String[] args) {
+        Map<String,String> testMap = new HashMap<>();
+        testMap.put("as","as");
+        testMap.put("as1","as");                // hash 3122
+        testMap.put("as2","as");
+        testMap.put("as3","as");
+        testMap.put("as4","as");
+        testMap.put("as5","as");
+        testMap.put("as6","as");
+        testMap.put("as7","as");
+        testMap.put("as8","as");
+        testMap.put("as9","as");
+        testMap.put("as10","as");
+        testMap.put("as11","as");
+        testMap.put("as12","as");
+        testMap.put("as13","as");
+        testMap.put("as14","as");
+        testMap.put("as15","as");
+        testMap.put("as16","as");
+        testMap.put("as17","as");
+        testMap.put("as18","as");
+        testMap.put("as19","as");
+        testMap.put("as20","as");
+        testMap.put("as21","as");
+        testMap.put("as22","as");
+        testMap.put("as23","as");
+        testMap.put("as24","as");
+        testMap.put("as25","as");
+        testMap.put("as26","as");
+        testMap.put("as27","as");
+        testMap.put("as28","as");
+        System.out.println(testMap);
+    }
+}
